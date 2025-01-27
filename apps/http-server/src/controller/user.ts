94aab08d9c3989ec11c0e923c5215e8e8eb15b69
Debabrata Sarkar <sarkardebu087@gmail.com>
2025-01-27 15:23:56 +0530

    creating simple be code

---
+++ apps/http-server/src/controller/user.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+const signup =(req: Request, res: Response)=>{
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.username;
+
+    res.json({
+        username,
+        email
+    })
+}
+
+const signin = (req: Request, res: Response)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    res.json({
+        email
+    })
+}
+
+export default {signup, signin};
--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
-
+import appRouter from "./routes/index";
 const app = express();
+
+app.use("/api/v1", appRouter);
 
 app.listen(3002);
